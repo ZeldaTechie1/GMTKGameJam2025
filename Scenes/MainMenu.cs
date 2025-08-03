91e9633831073eef05d728276bb3f4ba50ea77b3
Zeldatechie <jesrey4@outlook.com>
2025-08-02 23:16:13 -0400

    Lots of fixes and whistle hoooray

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -4,10 +4,24 @@ using System;
 public partial class MainMenu : Control
 {
     [Export] Button playButton;
+    [Export] Button exitButton;
+    [Export] Button skipButton;
 
     public override void _Ready()
     {
         playButton.ButtonDown += PlayButton_ButtonDown;
+        exitButton.ButtonDown += ExitButton_ButtonDown;
+        skipButton.ButtonDown += SkipButton_ButtonDown;
+    }
+
+    private void SkipButton_ButtonDown()
+    {
+        GetTree().ChangeSceneToFile("res://Scenes/PlayerControllerTest.tscn");
+    }
+
+    private void ExitButton_ButtonDown()
+    {
+        GetTree().Quit();
     }
 
     private void PlayButton_ButtonDown()
